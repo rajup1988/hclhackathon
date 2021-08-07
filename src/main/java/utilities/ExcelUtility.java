b75a709c68cff1a1dd4901d53a0a093a9245b0e1
sowmiya purushothaman <sowme76@gmail.com>
2021-08-07 15:38:59 +0800

    Excel changes

--- src/main/java/utilities/ExcelUtility.java
+++ src/main/java/utilities/ExcelUtility.java
@@ -27,46 +27,48 @@ public class ExcelUtility {
     @Test
     public static void Write(String content)
     {
-    	/*File checkFile = new File(FILE_NAME);
-    	if(checkFile.exists()) {
-    		System.out.print("Exist");
+    	File checkFile = new File(FILE_NAME);
+    	if(!checkFile.exists()) {
+    		try(FileOutputStream out = new FileOutputStream(
+    				new File(FILE_NAME));Workbook workbook = new XSSFWorkbook();) {
+    			Sheet spreadsheet = workbook.createSheet("HclHackathon");
+    			spreadsheet.createRow(0).createCell(0).setCellValue(content);
+    			workbook.write(out);
+    		} catch (Exception e) {
+    			e.printStackTrace();
+    		}
     	} else {
-    		FileOutputStream out = new FileOutputStream(
-	                new File(FILE_NAME));
-	        workbook.write(out);
-	        out.close();
-    	}*/
-		try (FileInputStream excelFile = new FileInputStream(new File(FILE_NAME));Workbook workbook = new XSSFWorkbook(excelFile)){
-	        Sheet spreadsheet = workbook.getSheetAt(0);
-	        Iterator<Row> iterator = spreadsheet.iterator();
-	        int rowCount = 0;
-	        while (iterator.hasNext()) {
+    		try (FileInputStream excelFile = new FileInputStream(new File(FILE_NAME));Workbook workbook = new XSSFWorkbook(excelFile)){
+    	        Sheet spreadsheet = workbook.getSheetAt(0);
+    	        Iterator<Row> iterator = spreadsheet.iterator();
+    	        int rowCount = 0;
+    	        while (iterator.hasNext()) {
 
-                Row currentRow = iterator.next();
-                Iterator<Cell> cellIterator = currentRow.iterator();
-                while (cellIterator.hasNext()) {
-                	Cell currentCell = cellIterator.next();
-                    if (currentCell.getCellType() == CellType.STRING) {
-                    	rowCount += 1;
+                    Row currentRow = iterator.next();
+                    Iterator<Cell> cellIterator = currentRow.iterator();
+                    while (cellIterator.hasNext()) {
+                    	Cell currentCell = cellIterator.next();
+                        if (currentCell.getCellType() == CellType.STRING) {
+                        	rowCount += 1;
+                        }
+                    	System.out.println(rowCount);
                     }
-                	System.out.println(rowCount);
                 }
-            }
-	        
-	        if(rowCount >= 0) {
-	        	spreadsheet.createRow(rowCount).createCell(0).setCellValue(content);
-	        }
-            
-	        FileOutputStream out = new FileOutputStream(
-	                new File(FILE_NAME));
-	        workbook.write(out);
-	        out.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-        
+    	        
+    	        if(rowCount >= 0) {
+    	        	spreadsheet.createRow(rowCount).createCell(0).setCellValue(content);
+    	        }
+                
+    	        FileOutputStream out = new FileOutputStream(
+    	                new File(FILE_NAME));
+    	        workbook.write(out);
+    	        out.close();
+    		} catch (FileNotFoundException e) {
+    			e.printStackTrace();
+    		} catch (IOException e) {
+    			e.printStackTrace();
+    		}
+    	}
     }
     
     public static void main(String[] args) {
